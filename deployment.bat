56ba4a5b404b1aaa7dce5b67c521a243cd4e7417
Pablo Prietz <pablo@prietz.org>
2020-10-26 18:24:12 +0100

    deployment: Build wheel without build-isolation

--- deployment.bat
+++ deployment.bat
@@ -1,3 +1,3 @@
 REM Change --ffmpeg-dir argument as necessary
 python setup.py clean --all build_ext --inplace --ffmpeg-dir=C:\work\ffmpeg-4.0-win64-dev -c msvc
-pip wheel .
+pip wheel . --no-build-isolation
